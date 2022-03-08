cdc33839d7ab3bd838c10b13799f929342a6b523
Michael Silva <michael.steam@outlook.com>
2022-03-07 22:34:29 -0300

    Adicionar arquivos de projeto.

---
+++ Ex3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor do raio do círculo: ");
+            double raio = Convert.ToDouble(Console.ReadLine());
+            double area = 3.14 * (raio * raio);
+            Console.WriteLine("O valor da área do círculo é: {0}", area);
+            Console.ReadLine();
+        }
+        
+    }
+}
